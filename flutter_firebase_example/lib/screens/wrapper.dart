6a09fc69abde1cd510a1e6852593082f86aadba0
Muhammad Imoeb <muhammad@diversition.co.th>
2020-10-15 17:32:05 +0700

    กำหนดโครงสร้างโปรเจคเบื้องต้น

---
+++ flutter_firebase_example/lib/screens/wrapper.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_firebase_example/screens/home/home.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // return หน้าแรก หรือ auth widget
+    return Home();
+  }
+}
